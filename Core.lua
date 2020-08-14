a0e722bd38312af27c8d4c99b9b1e413cebd071e
Joshua Harms <joshua@nozzlegear.com>
2020-08-14 13:02:56 -0500

    Add /am and /arenamarker commands

--- Core.lua
+++ Core.lua
@@ -1,12 +1,13 @@
-local function MarkParty(self, event, isInitialLogin, isReloadingUi)    
+local function MarkParty(skipInstanceCheck)    
     local function MaybeMarkUnit(unitType, iconIndex) 
+        local name = GetUnitName(unitType)
         -- First check if the unit is already marked
         local unitRaidTargetIcon = GetRaidTargetIndex(unitType)
 
         if unitRaidTargetIcon == nil then 
             SetRaidTarget(unitType, iconIndex) 
+            print("ArenaMarker: Marked " ..name.. " with raid icon " .. tostring(iconIndex) .. ".")
         else 
-            local name = GetUnitName(unitType)
             print("ArenaMarker: " .. name .. " is already marked. Skipping.")
         end
     end
@@ -17,7 +18,7 @@ local function MarkParty(self, event, isInitialLogin, isReloadingUi)
 
     -- Only mark players if the player is in arena 
     -- TODO: will this work when the player is the first to load in? Party may appear empty?
-    if instanceType == "arena" then 
+    if instanceType == "arena" or skipInstanceCheck == true then 
         MaybeMarkUnit("player", 2)
 
         if UnitExists("party1") then 
@@ -35,4 +36,15 @@ end
 local eventFrame = CreateFrame("Frame")
 -- PLAYER_ENTERING_WORLD is fired when user logs in, reloads UI, or goes through a loading screen (zone change)
 eventFrame:RegisterEvent("PLAYER_ENTERING_WORLD")
-eventFrame:SetScript("OnEvent", MarkParty)
+eventFrame:SetScript("OnEvent", function (self, event, isInitialLogin, isReloadingUi)
+    MarkParty(false)
+end)
+
+SLASH_ARENAMARKER1, SLASH_ARENAMARKER2 = "/am", "arenamarker"
+function SlashCmdList.ARENAMARKER(msg, editBox)
+    if msg == nil or msg == "" then 
+        MarkParty(true)
+    else 
+        print("Usage: /am or /arenamarker to mark friendly players.")
+    end
+end
